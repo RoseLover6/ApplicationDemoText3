cef534ea51d409b2f4a445ce6d8c5cd8c4cfceeb
RoseLover6 <fjyou2000@163.com>
2022-04-28 15:47:40 +0800

    Youfujian code

--- app/src/main/java/com/example/applicationdemotext3/ResultFragment.java
+++ app/src/main/java/com/example/applicationdemotext3/ResultFragment.java
@@ -7,15 +7,6 @@ import android.view.ViewGroup;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ScrollView;
-import android.widget.TextView;
-
-import androidx.fragment.app.Fragment;
-
 public class ResultFragment extends android.app.Fragment {
     String[] strings = new String[]{"您可在此设置用户信息","您可在此进行音量设置","您可在此进行安全设置",
             "您可在此设置亮度","其他设置"};
